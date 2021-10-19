b08b18b96e8bf2bcc2c9b85d653206f10e480bc9
Moki <mokiong1427@gmail.com>
2021-10-19 22:35:00 +0800

    Create Modal component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Todo from './components/Todo';
+import Modal from './components/Modal';
+import Backdrop from './components/Backdrop';
 
 function App() {
     return (
         <div>
             <h1>My todos</h1>
             <Todo text="learn react" />
-            <Todo />
-            <Todo />
+            <Todo text="Explore react" />
+            <Todo text="Master react" />
+            <Modal />
+            <Backdrop />
         </div>
     );
 }
