4f19d3c782b12dfeed061fb8d454335887f0a481
Moki <mokiong1427@gmail.com>
2021-10-19 22:26:16 +0800

    Make dynamic props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <div>
             <h1>My todos</h1>
-            <Todo />
+            <Todo text="learn react" />
             <Todo />
             <Todo />
         </div>
