2024d735bad5975ac48f5405b7b3ef3332e2e3d7
Moki <mokiong1427@gmail.com>
2021-10-19 23:30:07 +0800

    Create meetups component

--- src/pages/AllMetups.js
+++ src/pages/AllMetups.js
@@ -1,3 +1,5 @@
+import MeetupList from '../components/meetups/MeetupList';
+
 const DUMMY_DATA = [
     {
         id: 'm1',
@@ -21,11 +23,7 @@ function AllMeetups() {
     return (
         <section>
             <h1>Allmetups</h1>
-            <ul>
-                {DUMMY_DATA.map((meetup) => {
-                    return <li key={meetup.id}>{meetup.title}</li>;
-                })}
-            </ul>
+            <MeetupList meetups={DUMMY_DATA} />
         </section>
     );
 }
